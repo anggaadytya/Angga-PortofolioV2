2256e93fe2b645e26178e02b06bba56c535502ee
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2023-11-17 18:33:04 +0700

    feat/layout : test layout

---
+++ common/components/layouts/index.tsx
@@ -0,0 +1,78 @@
+"use client"
+import React, { Children } from "react";
+
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+
+    <div className="flex h-screen">
+      {/* Navbar pada tampilan desktop */}
+      <nav className="hidden sm:block bg-gray-800 text-gray-100 w-64">
+        {/* Isi sidebar di sini */}
+        Sidebar Desktop
+      </nav>
+
+      {/* Konten Utama */}
+      <main className="flex-1 overflow-y-auto md:p-5">
+        {/* Navbar untuk tampilan mobile */}
+        <nav className="sm:hidden bg-gray-800 text-gray-100 sticky top-0 z-50">
+          <div className="flex justify-between items-center px-4 py-4">
+            <h1 className="text-xl font-bold">Your App</h1>
+            <button
+              onClick={toggleSidebar}
+              className="text-white focus:outline-none focus:text-white"
+            >
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {isSidebarOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+        </nav>
+
+        {/* Konten */}
+        {children}
+      </main>
+
+      {/* Sidebar pada tampilan mobile */}
+      <aside
+        className={`fixed inset-y-0 left-0 z-50 bg-gray-800 text-gray-100 w-64 sm:hidden ${
+          isSidebarOpen ? 'block' : 'hidden'
+        }`}
+      >
+        {/* Isi sidebar di sini */}
+        Sidebar Mobile
+      </aside>
+    </div>
+ 
+
+  );
+}
---
+++ modules/home/components/Home.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+const Home = () => {
+  return (
+    <div className="h-[200vh]">
+      <div className="bg-red-500 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-600 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-700 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-800 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-900 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-400 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-300 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-200 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-100 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-500 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-600 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-700 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-800 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-900 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-400 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-500 w-[100vw] h-[20vh] text-white text-center">halo</div>
+      <div className="bg-red-600 w-[100vw] h-[20vh] text-white text-center">halo</div>
+    </div>
+  );
+};
+
+export default Home;
