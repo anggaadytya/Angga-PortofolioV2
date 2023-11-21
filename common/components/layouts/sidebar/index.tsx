e1b9cb27cb4b7167282f4c248574b23dde79ca3c
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2023-11-21 16:11:38 +0700

    feat/sidebar : update padding sidebar

--- common/components/layouts/sidebar/index.tsx
+++ common/components/layouts/sidebar/index.tsx
@@ -10,7 +10,7 @@ import Copyright from '@/common/components/elements/Copyright'
 export default function Sidebar() {
   const filterdMenu = MENU_ITEMS?.filter(item => item?.isShow)  
   return (
-    <div className='fixed md:bg-white inset-y-0 left-0 transition-all duration-300 flex flex-col md:px-2 md:h-screen md:py-4'>
+    <div className='fixed md:bg-white inset-y-0 left-0 transition-all duration-300 flex flex-col md:px-2 md:h-screen md:py-8'>
         <Profile/>
         <nav>
             <Breakline/>
