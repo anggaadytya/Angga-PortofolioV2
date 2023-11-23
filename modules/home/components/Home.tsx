0508623f36d7fad1958f0f4949e03e7927bc8cd4
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2023-11-23 19:06:25 +0700

    feat/home : update home

--- modules/home/components/Home.tsx
+++ modules/home/components/Home.tsx
@@ -1,6 +1,8 @@
-import SplashScreen from "@/common/components/layouts/sidebar/Splash";
+"use client";
 import Breakline from "@/common/components/elements/Breakline";
 import Title from "@/common/components/elements/Title";
+
+import { House } from "@phosphor-icons/react";
 
 import React from "react";
 import Github from "./Github";
@@ -9,7 +11,13 @@ const Home = () => {
   return (
     <>
       <Breakline />
-      <Title title="Home" />
+      <Title title="Home" icons={<House size={20} />} />
+      <p className="text-sm font-light text-zinc-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+        inventore ipsam laudantium itaque, dicta laboriosam cupiditate
+        doloremque facere! Facilis perferendis repellendus placeat aliquid
+        molestiae at accusamus, tenetur dolore sapiente tempora!
+      </p>
       <Breakline />
       <Github />
       <Breakline />
