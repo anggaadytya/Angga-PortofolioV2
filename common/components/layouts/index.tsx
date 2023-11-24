967fa98e66af8c353047004f5915586f5a30291c
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2023-11-24 16:18:47 +0700

    feat/layout : update animate

--- common/components/layouts/index.tsx
+++ common/components/layouts/index.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import SplashScreen from "@/common/components/layouts/sidebar/Splash";
 import { usePathname, useSearchParams } from "next/navigation";
 import React from "react";
 import Sidebar from "./sidebar";
-
-import { usesplash } from "@/store/splash";
+import { motion } from "framer-motion";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -16,41 +14,39 @@ export default function Layout({ children }: LayoutProps) {
   const searchParams = useSearchParams();
   const readMode = searchParams.get("read-mode");
 
-  const { loading, notLoading } = usesplash();
-
   const hideSidebar =
     ["/me", "/board"].includes(pathName) || readMode === "true";
 
-  React.useEffect(() => {
-    const times = setTimeout(() => {
-      notLoading();
-    }, 1000);
-
-    return () => clearTimeout(times);
-  }, []);
-
   return (
-    <div
-      className={`flex h-screen ${
-        loading ? "justify-center items-center" : ""
-      }`}
-    >
-      {loading ? (
-        <SplashScreen />
-      ) : (
-        <>
-          <div className="flex flex-col md:flex-row md:w-[26rem] lg:w-[23rem] justify-between md:gap-5">
-            {!hideSidebar && (
-              <header>
-                <Sidebar />
-              </header>
-            )}
-          </div>
-          <main className="transition-all scroll-smooth duration-300 w-full md:min-h-screen no-scrollbar">
-            {children}
-          </main>
-        </>
-      )}
+    <div className={`flex h-screen`}>
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20
+        }}
+        className="flex flex-col md:flex-row md:w-[26rem] lg:w-[23rem] justify-between md:gap-5"
+      >
+        {!hideSidebar && (
+          <header>
+            <Sidebar />
+          </header>
+        )}
+      </motion.div>
+      <motion.main
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping: 10
+        }}
+        className="transition-all scroll-smooth duration-300 w-full md:min-h-screen no-scrollbar"
+      >
+        {children}
+      </motion.main>
     </div>
   );
 }
