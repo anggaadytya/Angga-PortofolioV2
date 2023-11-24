acab7d760f9f9e995931df09fcab0d24d4543eb4
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2023-11-24 15:54:32 +0700

    feat/splash : update timer

--- common/components/layouts/index.tsx
+++ common/components/layouts/index.tsx
@@ -24,7 +24,7 @@ export default function Layout({ children }: LayoutProps) {
   React.useEffect(() => {
     const times = setTimeout(() => {
       notLoading();
-    }, 1500);
+    }, 1000);
 
     return () => clearTimeout(times);
   }, []);
